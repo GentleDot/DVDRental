9c5243b466963f8e2baffe7997f9aa66bf660c0e
gentledot <gentledot.wp@gmail.com>
2017-07-27 17:55:37 +0900

    DVDRental_storage - SQL과 VO 생성

---
+++ src/main/java/net/gentledot/rental/vo/StorageVO.java
@@ -0,0 +1,78 @@
+package net.gentledot.rental.vo;
+
+//@Data
+public class StorageVO extends PaginationVO {
+	private String  stId;
+	private String  stGetdate;
+	private String	pId;
+	private String	stStatus;
+	private String	stWastedate;
+	private String	stWastecost;
+	private String	stWasteReason;
+
+	public String getStId() {
+		return stId;
+	}
+
+	public void setStId(String stId) {
+		this.stId = stId;
+	}
+
+	public String getStGetdate() {
+		return stGetdate;
+	}
+
+	public void setStGetdate(String stGetdate) {
+		this.stGetdate = stGetdate;
+	}
+
+	public String getpId() {
+		return pId;
+	}
+
+	public void setpId(String pId) {
+		this.pId = pId;
+	}
+
+	public String getStStatus() {
+		return stStatus;
+	}
+
+	public void setStStatus(String stStatus) {
+		this.stStatus = stStatus;
+	}
+
+	public String getStWastedate() {
+		return stWastedate;
+	}
+
+	public void setStWastedate(String stWastedate) {
+		this.stWastedate = stWastedate;
+	}
+
+	public String getStWastecost() {
+		return stWastecost;
+	}
+
+	public void setStWastecost(String stWastecost) {
+		this.stWastecost = stWastecost;
+	}
+
+	public String getStWasteReason() {
+		return stWasteReason;
+	}
+
+	public void setStWasteReason(String stWasteReason) {
+		this.stWasteReason = stWasteReason;
+	}
+}
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
